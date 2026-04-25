a288bde7a04e9041862f770f1ba43e0527a619bd
JulianStone <julian@shiplogic.com>
2026-04-25 19:55:01 +0200

    Restore w-full to quick add transaction button for full width

--- frontend/src/components/QuickAddTransaction.tsx
+++ frontend/src/components/QuickAddTransaction.tsx
@@ -63,6 +63,7 @@ export default function QuickAddTransaction({ onTransactionAdded }: QuickAddTran
     return (
       <Button
         onClick={() => setIsOpen(true)}
+        className="w-full"
         size="lg"
       >
         <Plus className="h-4 w-4 mr-2" />
