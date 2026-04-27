ce29f35af705499c5e8e9a2b1261f2aa0f468a3e
JulianStone <julian@shiplogic.com>
2026-04-27 23:30:11 +0200

    Change Column to ColumnExpr in QueryAccounts to ensure dashboard_layout is selected

--- backend/core/db/db_accounts.go
+++ backend/core/db/db_accounts.go
@@ -14,7 +14,7 @@ func QueryAccounts(accountID *int64) ([]types.Account, int, error) {
 	userID := appcontext.GetUserID(context.Background())
 
 	query := db.NewSelect().Model(&accounts).
-		Column("a.*").
+		ColumnExpr("a.*").
 		Where("a.deleted_date IS NULL")
 
 	// If user is authenticated, include their own accounts and shared accounts
