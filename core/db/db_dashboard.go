efb9c5fec2a8631d8083143e7f8c793208f7c070
JulianStone <julian@shiplogic.com>
2026-05-01 14:16:06 +0200

    Use manual Join with table alias for recent transactions

--- core/db/db_dashboard.go
+++ core/db/db_dashboard.go
@@ -63,8 +63,7 @@ func GetDashboardSummary(accountID int64, from time.Time, to time.Time) (*Dashbo
 	// Recent transactions - filter by date range
 	var recentTrans []types.Transaction
 	err = db.NewSelect().
-		Model((*types.Transaction)(nil)).
-		TableExpr("transactions").
+		Model(&recentTrans).
 		Where("account_id = ?", accountID).
 		Where("deleted_date IS NULL").
 		Where("date >= ?", from).
